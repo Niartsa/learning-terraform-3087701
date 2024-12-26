a3e1fe3d91ff207d33afe39271f57ec8d65ab103
Niartsa <ronaldlizano1010@gmail.com>
2024-12-26 16:27:34 -0500

    Load balancer

--- main.tf
+++ main.tf
@@ -46,13 +46,12 @@ resource "aws_instance" "blog" {
 }
 
 module "blog_alb" {
-  source  = "terraform-aws-modules/alb/aws"
-  version = "~> 6.0" 
+  source = "terraform-aws-modules/alb/aws"
 
   name            = "blog-alb"
   vpc_id          = module.blog_vpc.vpc_id
   subnets         = module.blog_vpc.public_subnets
-  security_groups = module.blog_sg.security_group_id
+  security_groups = [module.blog_sg.security_group_id]
 
   target_groups = {
     ex-instance = {
@@ -60,26 +59,21 @@ module "blog_alb" {
       protocol         = "HTTP"
       port             = 80
       target_type      = "instance"
-      
-      targets = {
-        my target = {
-          target_id = aws_instance.blog.id
-          port      = 80
-        }
-      }
+      target_id        = aws_instance.blog.id
     }
   }
 
-  http_tcp_listeners = [
-    {
+  listeners = {
+    ex-http-redirect = {
       port               = 80
       protocol           = "HTTP"
       target_group_index = 0
     }
-  ]
+  }
 
   tags = {
     Environment = "dev"
+    Project     = "Example"
   }
 }
 
