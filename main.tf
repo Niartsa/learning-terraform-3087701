fd3407736d2e1691ab750ef929c4f8b2cade4d17
Niartsa <ronaldlizano1010@gmail.com>
2024-12-26 16:34:14 -0500

    Load balancer

--- main.tf
+++ main.tf
@@ -63,17 +63,16 @@ module "blog_alb" {
     }
   }
 
-  http_tcp_listeners = [
-    {
+  listeners = {
+    http_tcp_listeners = {
       port               = 80
       protocol           = "HTTP"
       target_group_index = 0
     }
-  ]
+  }
 
   tags = {
     Environment = "dev"
-    Project     = "Example"
   }
 }
 
