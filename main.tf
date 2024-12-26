15c5e8ce2bb56a93b7b58006eb939164adc71381
Niartsa <ronaldlizano1010@gmail.com>
2024-12-26 16:19:14 -0500

    Load balancer

--- main.tf
+++ main.tf
@@ -46,7 +46,8 @@ resource "aws_instance" "blog" {
 }
 
 module "blog_alb" {
-  source = "terraform-aws-modules/alb/aws"
+  source  = "terraform-aws-modules/alb/aws"
+  version = "~> 6.0" 
 
   name            = "blog-alb"
   vpc_id          = module.blog_vpc.vpc_id
