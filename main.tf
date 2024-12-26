7ea17b737cbc081d993f7da6c53f7bc453ec82e9
Niartsa <ronaldlizano1010@gmail.com>
2024-12-26 17:37:31 -0500

    Alb

--- main.tf
+++ main.tf
@@ -35,14 +35,14 @@ module "blog_vpc" {
 
 module "blog_autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
-  version = "8.0.1"
+  version = "6.5.2"
 
   name     = "blog"
   min_size = 1
   max_size = 2
 
   vpc_zone_identifier = module.blog_vpc.public_subnets
-  load_balancers      = [module.blog_alb.target_group_arns]
+  target_group_arns      = [module.blog_alb.target_group_arns]
   security_groups     = [module.blog_sg.security_group_id]
 
   image_id = data.aws_ami.app_ami.id
